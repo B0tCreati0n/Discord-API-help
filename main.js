d1c4f6c13ebb340435a98f5af7424231ae1ec589
B0tCreati0n <109825490+B0tCreati0n@users.noreply.github.com>
2023-02-10 16:44:13 +0100

    main.js
    the main file

---
+++ main.js
@@ -0,0 +1,30 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.DirectMessages
+  ]
+});
+
+const prefix = '!';
+
+//on ready should send B0t Creati0n's Helper
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+//commands
+//test
+client.on("message", async message => {
+  if (message.channel.type === "dm") {
+    console.log(`Received a DM from ${message.author.tag}: ${message.content}`);
+    if (message.content === "ping") {
+      const startTime = Date.now();
+      await message.channel.send("Pong!").then((msg) => {
+        const endTime = Date.now();
+        msg.edit(`Pong! (Response time: ${endTime - startTime}ms)`);
+      });
+    }
+  }
+});
+//token: removed :)
+client.login("removed :)");
